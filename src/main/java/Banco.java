4b3041e2a4ab1797690e3546572328effb78b02f
daanielxyz <josed.valenciah@uqvirtual.edu.co>
2025-02-20 19:13:48 -0500

    Update 4
    Clase "Banco" con los metodos de agregar, modificar y eliminar usuario

--- src/main/java/Banco.java
+++ src/main/java/Banco.java
@@ -16,6 +16,7 @@ public class Banco {
         String verde = "\u001B[92m";
         String oro = "\u001B[33m";
         String azul = "\u001B[34m";
+        String rojo = "\u001B[31m";
 
 
     //CONSTRUCTOR
@@ -67,10 +68,21 @@ public class Banco {
             }
             return texto;
         }
+        public static int leerEnteroValido(Scanner scanner, String mensaje) {
+            String reset = "\u001B[0m";
+            String negrita = "\u001B[1m";
+            String rojo = "\u001B[31m";
+            while (true) {
+                try {
+                    System.out.print(mensaje);
+                    return Integer.parseInt(scanner.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println(rojo+negrita+"Dato inválido. Por favor, ingrese un número entero."+reset);
+                }
+            }
+        }
 
         
-
-
     //METODO registrarUsuario
         public void registrarUsuario(String nombre, String direccion, String id, String correo, String contrasena){
             Usuario usuario = new Usuario("", "", "", "", "");
@@ -85,15 +97,82 @@ public class Banco {
 
     //METODO actualizarUsuario
         public void actualizarUsuario(ArrayList<Usuario> usuarios){
+            if (usuarios.isEmpty()) {
+                System.out.println(rojo+negrita+"No hay usuarios registrados."+reset);
+                return;
+            }
             System.out.println(negrita+azul+"Lista de usuarios registrados:"+reset );
             for (int i = 0; i<usuarios.size(); i++){
                 Usuario usuario = usuarios.get(i);
                 System.out.println(negrita+azul+i+"."+reset+oro+usuario.getNombre()+reset);
             }
-            System.out.println(azul+negrita+"Seleccione el índice del usuario para el cual quiere actualizar los datos: "+reset);
-            int indice =
+            int indice = leerEnteroValido(scanner, azul+negrita+"Seleccione el índice del usuario para el cual quiere actualizar los datos: "+reset);
+            if (indice < 0 || indice >= usuarios.size()) {
+                System.out.println(rojo+negrita+"Índice inválido."+reset);
+                return;
+            }
+            Usuario usuarioSeleccionado = usuarios.get(indice);
+
+            System.out.println(azul+negrita+"\nOpciones de gestión:"+reset);
+            System.out.println(azul+negrita+"1."+reset+oro+ "Cambiar nombre"+reset);
+            System.out.println(oro+negrita+"2."+reset+oro+ "Cambiar direccion"+reset);
+            System.out.println(oro+negrita+"3."+reset+oro+ "Cambiar numero de identificacion"+reset);
+            System.out.println(oro+negrita+"4."+reset+oro+ "Cambiar correo"+reset);
+            System.out.println(oro+negrita+"5."+reset+oro+ "Cambiar contraseña"+reset);
+            System.out.print(azul+negrita+"Seleccione una opción: "+reset);
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    String nuevoNombre = leerTextoValido(scanner,azul+"Ingrese el nombre nuevo: "+reset);
+                    usuarioSeleccionado.setNombre(nuevoNombre);
+                    System.out.println(negrita+verde+"Nuevo nombre registrado con éxito!");
+                    break;
+                case 2:
+                    String nuevaDireccion = leerTextoValido(scanner,azul+"Ingrese la direccion nuevo: "+reset);
+                    usuarioSeleccionado.setDireccion(nuevaDireccion);
+                    System.out.println(negrita+verde+"Nueva direccion registrada con éxito!");
+                    break;
+                case 3:
+                    String nuevoId = leerTextoValido(scanner,azul+"Ingrese el id nuevo: "+reset);
+                    usuarioSeleccionado.setId(nuevoId);
+                    System.out.println(negrita+verde+"Nuevo numero de identificacion registrado con éxito!");
+                    break;
+                case 4:
+                    String nuevoCorreo = leerTextoValido(scanner,azul+"Ingrese el correo nuevo: "+reset);
+                    usuarioSeleccionado.setCorreo(nuevoCorreo);
+                    System.out.println(negrita+verde+"Nuevo correo registrado con éxito!");
+                    break;
+                case 5:
+                    String nuevaContrasena = leerTextoValido(scanner, azul+"Ingrese la nueva contraseña: "+reset);
+                    usuarioSeleccionado.setContrasena(nuevaContrasena);
+                    System.out.println(negrita+verde+"Nueva contraseña registrada con éxito!");
+                    break;
+            }
         }
 
+
+    //METODO eliminarUsuario
+        public void eliminarUsuario(ArrayList<Usuario> usuarios){
+            if (usuarios.isEmpty()) {
+                System.out.println(rojo+negrita+"No hay usuarios registrados."+reset);
+                return;
+            }
+            System.out.println(negrita+azul+"Lista de usuarios registrados:"+reset );
+            for (int i = 0; i<usuarios.size(); i++){
+                Usuario usuario = usuarios.get(i);
+                System.out.println(negrita+azul+i+"."+reset+oro+usuario.getNombre()+reset);
+            }
+            int indice = leerEnteroValido(scanner, azul+negrita+"Seleccione el índice del usuario para eliminar: "+reset);
+            if (indice < 0 || indice >= usuarios.size()) {
+                System.out.println(rojo+negrita+"Índice inválido."+reset);
+                return;
+            }
+            Usuario usuarioSeleccionado = usuarios.get(indice);
+            usuarios.remove(usuarioSeleccionado);
+            System.out.println(verde+negrita+"Usuario eliminado con éxito!."+reset);
+        }
 
 
     //TOSTRING
